b6371950dbb7da94f6bdc46931b9e42617a6a1c0
mohamed-mohaimen <mohamedveron23@gmail.com>
2021-02-14 05:09:31 +0200

    add create project test case

--- tests/create_project_test.go
+++ tests/create_project_test.go
@@ -1,19 +1,16 @@
-package main
+package tests
 
 import (
-	"fmt"
-	"github.com/go-chi/chi"
-	"github.com/mohamedveron/project_management/api"
 	"github.com/mohamedveron/project_management/domains"
 	"github.com/mohamedveron/project_management/service"
-	"net/http"
+	"testing"
 )
 
+	var projectsDB = make(map[string]domains.Project)
+	var employeesDB = make(map[string]domains.Employee)
 
-func main() {
 
-	// make a list of employees to do the operation instead of db
-	employee1 := domains.Employee{
+	var employee1 = domains.Employee{
 		ID:         "11aa",
 		FirstName:  "Peter",
 		LastName:   "Golm",
@@ -22,7 +19,7 @@ func main() {
 		Department: "Engineering",
 	}
 
-	employee2 := domains.Employee{
+	var employee2 = domains.Employee{
 		ID:         "33cc",
 		FirstName:  "Andreas",
 		LastName:   "Litt",
@@ -31,7 +28,7 @@ func main() {
 		Department: "Engineering",
 	}
 
-	employee3 := domains.Employee{
+	var employee3 = domains.Employee{
 		ID:         "58kk",
 		FirstName:  "Nina",
 		LastName:   "Wessels",
@@ -40,7 +37,7 @@ func main() {
 		Department: "HR",
 	}
 
-	employee4 := domains.Employee{
+	var employee4 = domains.Employee{
 		ID:         "t3t3",
 		FirstName:  "Mohamed",
 		LastName:   "Abdel Mohaimen",
@@ -50,7 +47,7 @@ func main() {
 	}
 
 	// make a list of projects to do the operation instead of db
-	project1 := domains.Project{
+	var project1 = domains.Project{
 		ID:           "11bb",
 		Name:         "project management",
 		Owner:        domains.Employee{},
@@ -59,34 +56,23 @@ func main() {
 		Participants: []domains.Employee{},
 	}
 
-	projectsDB := make(map[string]domains.Project)
-	employeesDB := make(map[string]domains.Employee)
 
-	employeesDB["11aa"] = employee1
-	employeesDB["33cc"] = employee2
-	employeesDB["58kk"] = employee3
-	employeesDB["t3t3"] = employee4
+func TestCreateProject(t *testing.T){
 
-	projectsDB["11bb"] = project1
-
-	serviceLayer := service.NewService(projectsDB, employeesDB)
-	server := api.NewServer(serviceLayer)
-
-	// prepare router
-	r := chi.NewRouter()
-	r.Route("/", func(r chi.Router) {
-		r.Mount("/", api.Handler(server))
-	})
-
-	srv := &http.Server{
-		Handler: r,
-		Addr:    ":8080",
+	project := domains.Project{
+		ID:           "",
+		Name:         "project1",
+		Owner:        domains.Employee{},
+		Progress:     0,
+		State:        "planned",
+		Participants: nil,
 	}
 
-	fmt.Println("server starting on port 8080...")
+	service := service.NewService(projectsDB, employeesDB)
 
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		fmt.Println("server failed to start", "error", err)
+	_ , err := service.CreateProject(project)
+
+	if err != nil {
+		t.Error("can't create project")
 	}
-
 }
